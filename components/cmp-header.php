7c78172af2a48d690d02611bedc40fd6203f08df
Danylo <antoni@everywhere.pl>
2023-04-24 14:23:57 +0200

    Header social

--- components/cmp-header.php
+++ components/cmp-header.php
@@ -1,8 +1,9 @@
 <header class="container mx-auto">
-    <div class="flex justify-between">
+    <div class="flex justify-between items-center">
         <a href="<?php echo get_home_url() ?>">
             <img src="<?php echo IMG . "logo.png" ?>"
-                 alt="#DOBRYHASZTAG">
+                 alt="#DOBRYHASZTAG"
+                 class="w-full max-w-72">
         </a>
         <nav>
             <ul class="flex list-none">
@@ -10,7 +11,7 @@
                 $menus_count = count($menu['menus']) - 1;
                 $menus_key = 0;
                 foreach($menu["menus"] as $el) : ?>
-                <li class="<?php echo $menus_key < $menus_count ? 'mr-10' : '' ?>">
+                <li class="<?php echo $menus_key < $menus_count ? 'mr-8' : '' ?>">
                     <?php get_template_part( CMP, "btn", array(
                         'content' => $el['title'],
                         'url' => $el['url'],
@@ -18,6 +19,23 @@
                     )) ?>
                 </li>
                 <?php $menus_key++; endforeach; ?>
+            </ul>
+        </nav>
+        <nav>
+            <ul class="flex list-none">
+                <?php $social = get_field("opt_social", "options"); 
+                $social_count = count($social) - 1;
+                $social_key = 0;
+                foreach($social as $el) : ?>
+                <li class="<?php echo $social_key < $social_count ? 'mr-4' : '' ?>">
+                    <?php get_template_part( CMP, "btn", array(
+                        'content' => file_get_contents($el['icon']),
+                        'url' => $el['link'],
+                        'class' => 'rounded-full bg-white p-3',
+                        'class_wrapper' => 'before:rounded-full'
+                    )) ?>
+                </li>
+                <?php $social_key++; endforeach; ?>
             </ul>
         </nav>
     </div>
