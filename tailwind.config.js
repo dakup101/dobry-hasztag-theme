7443fb4bfb4a113ea5e760fcc5feab00171c4f25
Danylo <antoni@everywhere.pl>
2023-04-24 11:33:15 +0200

    Tailwind - add project fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
     content: [
         './views/*.php',
+        './index.php',
+        './components/*.php'
     ],
     theme: {
         colors: {
             'dark': '#222222',
             'white': '#ffffff',
         },
+        extend: {
+            fontFamily: {
+                'sans' : ['Inter', 'sans-serif'], 
+                'sofia' : ['sofia-pro-soft', 'sans-serif']
+            }
+        }
     },
     plugins: [
     ],
