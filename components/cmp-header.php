522f4fd1056fe1299463a41930ec348e8b7efe45
Danylo <antoni@everywhere.pl>
2023-04-24 13:14:09 +0200

    Header

--- components/cmp-header.php
+++ components/cmp-header.php
@@ -1,3 +1,20 @@
-<header>
-
+<header class="container mx-auto">
+    <div class="flex justify-between">
+        <a href="<?php echo get_home_url() ?>">
+            <img src="<?php echo IMG . "logo.png" ?>" alt="#DOBRYHASZTAG">
+        </a>
+        <nav>
+            <ul>
+                <?php $menu = get_menu("primary"); foreach($menu["menus"] as $key => $el) : ?>
+                <li>
+                    <?php get_template_part( CMP, "btn", array(
+                        'content' => $el['title'],
+                        'url' => $el['url'],
+                        'class' => !empty($el['active']) ? 'bg-yellow translate-x-1.5 translate-y-1.5 events-none' : '', 
+                    )) ?>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        </nav>
+    </div>
 </header>
