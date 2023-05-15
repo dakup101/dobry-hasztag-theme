7974b1a25b63466220afc6502ba77b88d20a7a4b
Danylo <antoni@everywhere.pl>
2023-05-15 15:26:51 +0200

    social dialog handle

---
+++ src/ts/handlers/social-dialog-handle.ts
@@ -0,0 +1,32 @@
+export default function socialDialog_handle() {
+	const target = document.querySelector(".show-social");
+	const dialog = document.querySelector(
+		"[data-social-dialog]"
+	) as HTMLDialogElement;
+
+	if (!target || !dialog) return;
+
+	console.log("--- Social Dialog Handle ---");
+
+	const observerOpts = {
+		root: null,
+		rootMargin: "-100px 0px 100px 0px",
+		threshold: 0,
+	};
+
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				import(
+					/* webpackChunkName: "print" */ "../functions/social-dialog"
+				).then((module) => {
+					const action = module.default;
+					action();
+				});
+				observer.unobserve(target);
+			}
+		});
+	}, observerOpts);
+
+	observer.observe(target);
+}
