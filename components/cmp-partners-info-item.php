94986a676d2679196eb3ad9e80f2f4493689e5cb
Danylo <antoni@everywhere.pl>
2023-06-01 13:21:36 +0200

    RWD | Partners

--- components/cmp-partners-info-item.php
+++ components/cmp-partners-info-item.php
@@ -5,16 +5,16 @@
 $partner = $args["partner"] ?>
 
 <article class="neo-before mb-1.5 mr-1.5">
-    <div class="relative px-10 py-5 border-4 border-dark rounded-xl"
+    <div class="relative px-5 md:px-10 py-5 border-4 border-dark rounded-xl"
          style="background: <?php echo $partner["color"] ?>">
         <?php if ($partner["is_boss"]) : ?>
-        <div class="grid grid-cols-4 gap-10">
+        <div class="grid grid-cols-12 gap-y-10 md:gap-10">
             <section
-                     class="col-span-3 <?php echo ($args["key"] == 0 || $args["key"] % 2 == 0) ? "order-first" : "order-last" ?>">
+                     class="col-span-12 md:col-span-8 xl:col-span-9 <?php echo ($args["key"] == 0 || $args["key"] % 2 == 0) ? "order-first" : "order-first md:order-last" ?>">
                 <?php endif; ?>
 
                 <img src="<?php echo $partner["logo"] ?>"
-                     class=" h-20">
+                     class="h-10 md:h-20">
 
                 <div class="mt-10">
                     <?php echo wpautop( $partner["opis"] ) ?>
@@ -22,7 +22,8 @@ $partner = $args["partner"] ?>
 
                 <?php if ($partner["is_boss"]) : ?>
             </section>
-            <aside class="<?php echo ($args["key"] == 0 || $args["key"] % 2 == 0) ? "order-last" : "order-first" ?>">
+            <aside
+                   class="col-span-12 md:col-span-4 xl:col-span-3 <?php echo ($args["key"] == 0 || $args["key"] % 2 == 0) ? "order-last" : "order-last md:order-first" ?>">
                 <?php get_template_part( CMP, "partners-info-boss", $partner["boss"] ) ?>
             </aside>
         </div>
